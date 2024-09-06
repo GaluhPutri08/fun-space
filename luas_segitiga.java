b120da1adc2074019eb6e80d835476c017705ce4
GaluhPutri08 <galuh.saprilia@gmail.com>
2024-09-06 10:56:24 +0700

    First Github Project

---
+++ luas_segitiga.java
@@ -0,0 +1,15 @@
+public class luas_segitiga{
+
+   public static world main(string[] args) {
+   // deklarasi variabel
+   double alas ,tinggi,luas; 
+
+   //instalasi variabel
+   alas=5;
+   tinggi=10;
+   luas=0.5*alas*tinggi;
+
+   //cetak hasil
+   System.out.print("luas segitiga adalah"+luas);
+  }
+}
